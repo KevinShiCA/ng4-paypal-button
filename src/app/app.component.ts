9d6849d6e9227355d47f012e7364cb03ce981daa
Kevin Shi <kshi079@uottawa.ca>
2017-09-06 08:46:47 -0400

    Added paypal button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked } from '@angular/core';
+
+declare let paypal: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked {
   title = 'app';
+
+  private didPaypalRender: boolean = false;
+
+  private paypalConfig: any = {
+    env: 'sandbox',
+    client: {
+      sandbox: 'xxxxxxxxxx',
+      production: 'xxxxxxxxxx'
+    },
+    commit: true,
+    payment: (data, actions) => {
+      return actions.payment.create({
+        payment: {
+          transactions: [
+            { amount: { total: '1.00', currency: 'CAD' } }
+          ]
+        }
+      });
+    },
+    onAuthorize: (data, actions) => {
+      // show success page
+    }
+  };
+
+  ngAfterViewChecked() {
+    if(!this.didPaypalRender) {
+      this.loadPaypalScript().then(() => {
+        paypal.Button.render(this.paypalConfig, '#paypal-button');
+      });
+    }
+  }
+
+  loadPaypalScript(): Promise<any> {
+    this.didPaypalRender = true;
+    return new Promise((resolve, reject) => {
+      const scriptElement = document.createElement('script');
+      scriptElement.src = 'https://www.paypalobjects.com/api/checkout.js';
+      scriptElement.onload = resolve;
+      document.body.appendChild(scriptElement);
+    });
+  }
 }
